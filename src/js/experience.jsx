7f33dbe38eb54eea4f09f08f4e6a87619f9f02b4
Charana Nandasena <an15739@my.bristol.ac.uk>
2019-10-28 00:22:37 +0530

    stackhpc content

--- src/js/experience.jsx
+++ src/js/experience.jsx
@@ -31,9 +31,7 @@ export default class Experience extends React.Component {
                     </div>
                     <div className="pt-2 jumbotron-underline"> </div>
                     <ul className="pt-3">
-                        <li>Coffee</li>
-                        <li>Tea</li>
-                        <li>Milk</li>
+                        <li> Hello World </li>
                     </ul>
                 </div>
                 <div className="jumbotron jumbotron-style">
@@ -46,9 +44,34 @@ export default class Experience extends React.Component {
                     </div>
                     <div className="pt-2 jumbotron-underline"> </div>
                     <ul className="pt-3">
-                        <li>Coffee</li>
-                        <li>Tea</li>
-                        <li>Milk</li>
+                        <li> Work involved the development and improvement of serveral components of OpenStack Monasca. A opensource, micro-service based,
+                            high-throughput, performant monitoring solution. Take a look at its 
+                            <a href="https://wiki.openstack.org/wiki/Monasca#Comparisons_to_alternatives"> architecture </a>.
+                        </li>
+                        <li>
+                            Developed and tested a front-end 
+                            <a href="https://github.com/monasca/monasca-grafana"> plugin </a>
+                            for Grafana (a data visualisation tool) to present telemetry data from
+                            nodes on multiple cluster that are stored in Monasca's Datastore (an Apache Kafka message queue)
+                            and notify SysOps of potential server malfunction through Email, PagerDuty etc.
+                        </li>
+                        <li>
+                            Developed an distributed agent
+                            <a href="https://github.com/stackhpc/stackhpc-monasca-agent-plugins/tree/charana"> plugin </a>
+                            that runs periodically on nodes on cluster and gathers Slurm (a distributed workload manager) metrics
+                            , storing them relative to their timestamp in a Kafka message queue for asynchronous processing.
+                        </li>
+                        <li> 
+                            Yet onother Grafana
+                            <a href="https://github.com/stackhpc/slurm-visualisation-app"> plugin </a>
+                            to present the telemetry data from Slurm as both a time-series graph and an interactive gantt chart
+                            that provides SysOps both a birds eye view of the state of a Slurm cluster and the ability to drill
+                            down to individual workloads or nodes to troubleshoot malfunctions.
+                        </li>
+                        <li>
+                            Rendered the word cloud on StackHPC's 
+                            <a href="http://stackhpc.com/"> landing page </a>
+                        </li>
                     </ul>
                 </div>
                 <div className="jumbotron jumbotron-style">
